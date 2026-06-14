374d005dba60dc46c7bd8b0619bef0bf8fea7acb
Keyun_GitHub <125251980+Keyun-github@users.noreply.github.com>
2026-06-14 16:57:09 +0700

    BackEnd Fix

--- BackEnd/src/orders/dto/update-status.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, IsIn } from 'class-validator';
-import { OrderStatus } from '../order.entity.js';
-
-export class UpdateStatusDto {
-  @IsString()
-  @IsIn(Object.values(OrderStatus))
-  status: OrderStatus;
-}
