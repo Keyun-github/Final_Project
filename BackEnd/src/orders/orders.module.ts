85d6415dc05d0815b74c2130e3d2df2a53b1f995
Keyun_GitHub <125251980+Keyun-github@users.noreply.github.com>
2026-06-16 18:05:18 +0700

    fix bug

--- BackEnd/src/orders/orders.module.ts
+++ BackEnd/src/orders/orders.module.ts
@@ -3,13 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './order.entity.js';
 import { OrderItem } from './order-item.entity.js';
 import { Product } from '../products/product.entity.js';
+import { ProductVariant } from '../products/product-variant.entity.js';
 import { Driver } from '../drivers/driver.entity.js';
 import { DriversModule } from '../drivers/drivers.module.js';
 import { OrdersService } from './orders.service.js';
 import { OrdersController } from './orders.controller.js';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product, Driver]), DriversModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      Order,
+      OrderItem,
+      Product,
+      ProductVariant,
+      Driver,
+    ]),
+    DriversModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
